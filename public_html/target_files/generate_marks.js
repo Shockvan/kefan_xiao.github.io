a3b4565f1f939b9ee5b33c0f98d1ccfe9815bec4
Frank <shockvanbell@gmail.com>
2018-11-28 17:18:20 -0600

    Initial commit

---
+++ public_html/target_files/generate_marks.js
@@ -0,0 +1,21 @@
+function GenerateNumber(table_id) {
+  var table, rows, i, j, col;
+  var blanket1 = "[";
+  table = document.getElementById(table_id);
+  rows = table.getElementsByTagName("TR");
+  var digits_number = rows.length.toString().length;
+  for (i = 0; i < rows.length; i++) {
+    col = rows[i].getElementsByTagName("TD")[0];
+    var target = i + 1;
+    var target_string = target.toString();
+    var prefix = "";
+    for (j = 0; j < digits_number - target_string.length; j++) {
+      prefix = prefix.concat("0");
+    }
+    col.innerHTML = blanket1.concat(prefix, target_string, "]");
+  }
+}
+function GenerateForBody() {
+  GenerateNumber("journal_papers");
+  GenerateNumber("conference_papers");
+}
